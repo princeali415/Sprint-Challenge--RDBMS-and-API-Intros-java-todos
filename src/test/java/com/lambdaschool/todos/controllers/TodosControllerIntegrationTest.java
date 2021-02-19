05c4159c3f9474da4962d946925c47b84ea79994
Hussain Ali <ali.415code@gmail.com>
2021-02-19 02:31:26 -0800

    Fixed tests files

--- src/test/java/com/lambdaschool/todos/controllers/TodosControllerIntegrationTest.java
+++ src/test/java/com/lambdaschool/todos/controllers/TodosControllerIntegrationTest.java
@@ -3,7 +3,6 @@ package com.lambdaschool.todos.controllers;
 import com.lambdaschool.todos.TodosApplication;
 import com.lambdaschool.todos.TodosApplicationTests;
 import com.lambdaschool.todos.models.Todo;
-import com.lambdaschool.todos.models.Todos;
 import com.lambdaschool.todos.models.User;
 import com.lambdaschool.todos.services.TodosService;
 import com.lambdaschool.todos.services.UserService;
